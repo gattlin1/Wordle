184ed81e5c92e14b4885c07859f7e29c96b570bf
gattlin1 <gattlin1@live.missouristate.edu>
2022-04-06 21:08:28 -0400

    Fixed infinite loop. Moved the keyboard input handler to the Board component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Board from './components/Board/Board';
 import Keyboard from './components/Keyboard/Keyboard';
 
 function App() {
-  let [currentGuess, setCurrentGuess] = useState('');
+  const [guesses, setGuesses] = useState(['', '', '', '', '']);
   let [guessCount, setGuessCount] = useState(0);
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyboardInput);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyboardInput);
-    };
-  });
-
-  const handleKeyboardInput = (event: any) => {
-    const key = event.key;
-    const keyCode = event.keyCode;
-
-    if (key === 'Backspace') {
-      setCurrentGuess(currentGuess.slice(0, -1));
-    }
-    // Check to see if input is a letter.
-    if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
-      if (currentGuess.length < 5) {
-        setCurrentGuess((currentGuess += key));
-      }
-    }
-
-    if (key === 'Enter' && currentGuess.length === 5) {
-      setGuessCount(guessCount + 1);
-    }
-  };
 
   return (
     <div className='App'>
       <header>Wordle</header>
       <div className='board-container'>
-        <Board currentGuess={currentGuess} guessCount={guessCount} />
+        <Board
+          guesses={guesses}
+          setGuesses={setGuesses}
+          guessCount={guessCount}
+          setGuessCount={setGuessCount}
+        />
       </div>
       <div className='keyboard-container'>
         <Keyboard />
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,20 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import CharSquare from './CharSquare/CharSquare';
 import './Board.scss';
 
 interface BoardProps {
-  currentGuess: string;
+  guesses: string[];
+  setGuesses: Dispatch<SetStateAction<string[]>>;
   guessCount: number;
+  setGuessCount: Dispatch<SetStateAction<number>>;
 }
 
-function Board({ currentGuess, guessCount }: BoardProps) {
-  const [guesses, setGuesses] = useState(['', '', '', '', '']);
-
+function Board({ guesses, setGuesses, guessCount, setGuessCount }: BoardProps) {
   useEffect(() => {
+    document.addEventListener('keydown', handleKeyboardInput);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboardInput);
+    };
+  });
+
+  const handleKeyboardInput = (event: any) => {
+    const key: string = event.key;
+    const keyCode = event.keyCode;
+
+    let currentGuess = guesses[guessCount];
+
+    if (key === 'Backspace') {
+      if (currentGuess.length > 0) {
+        currentGuess = currentGuess.slice(0, -1);
+        updatedGuesses(currentGuess);
+      }
+    }
+    // Check to see if input is a letter.
+    if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
+      if (currentGuess.length < 5) {
+        currentGuess += key.toUpperCase();
+        updatedGuesses(currentGuess);
+      }
+    }
+
+    if (key === 'Enter' && currentGuess.length === 5) {
+      setGuessCount(guessCount + 1);
+    }
+  };
+
+  const updatedGuesses = (word: string) => {
     const tempGuesses = guesses.slice();
-    tempGuesses.splice(guessCount, 1, currentGuess);
+    tempGuesses.splice(guessCount, 1, word);
     setGuesses(tempGuesses);
-  }, []);
+  };
 
   const createGuess = (word: string) => {
     const guess = [];
